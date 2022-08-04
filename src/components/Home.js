438ffce1f9848a8f22e2870f5602e2ec25ab81e1
Faiz Mohammad Hafidza <faizmohhafidza@gmail.com>
2022-08-04 16:06:12 +0700

    added prop types

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import React from "react";
 export default function Home(props) {
     const { isLoggedIn } = props;
@@ -5,3 +6,11 @@ export default function Home(props) {
     if (isLoggedIn) return <div>Welcome back, pal</div>;
     return <div>Please login</div>;
 }
+
+Home.propTypes = {
+    isLoggedIn: PropTypes.bool.isRequired,
+};
+
+Home.defaultProps = {
+    isLoggedIn: false,
+};
